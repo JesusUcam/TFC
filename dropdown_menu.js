58c27a0dc5fa1ce3898bc4bb05d7de4a3bf06847
JesusUcam <jmartinez84@alu.ucam.edu>
2024-04-17 22:41:51 +0200

    Landin Page creada

---
+++ dropdown_menu.js
@@ -0,0 +1,12 @@
+let toggleBtn = document.querySelector('.toggle_btn');
+let toggleBtnIcon = document.querySelector('.toggle_btn i');
+let dropDownMenu = document.querySelector('.dropdown_menu');
+
+toggleBtn.addEventListener("click", function() {
+    dropDownMenu.classList.toggle('open')
+    let isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+})
