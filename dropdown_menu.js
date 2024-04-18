62c1e1c16ed18f785af0bb98d171b3054500b4ec
AlejandroC32 <156315585+AlejandroC32@users.noreply.github.com>
2024-04-18 22:52:42 +0200

    Update dropdown_menu.js

--- dropdown_menu.js
+++ dropdown_menu.js
@@ -2,6 +2,9 @@ let toggleBtn = document.querySelector('.toggle_btn');
 let toggleBtnIcon = document.querySelector('.toggle_btn i');
 let dropDownMenu = document.querySelector('.dropdown_menu');
 
+// Comentario de prueba
+
+
 toggleBtn.addEventListener("click", function() {
     dropDownMenu.classList.toggle('open')
     let isOpen = dropDownMenu.classList.contains('open')
