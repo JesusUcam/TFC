a4acf7d0176e76481be9018510baef74a0430601
JesusUcam <jmartinez84@alu.ucam.edu>
2024-04-27 13:54:07 +0200

    front_controlador

---
+++ controlador/front_controlador.php
@@ -0,0 +1,31 @@
+<?php
+function console_log($data) {
+    echo '<script>';
+    echo 'console.log(' . json_encode($data) . ')';
+    echo '</script>';
+}
+
+define('CONTROLLERS_FOLDER', "controlador/");
+define('DEFAULT_CONTROLLER', "inicio"); //El controlador por defecto será la página de inicio
+define('DEFAULT_ACTION', "home");
+
+$controller = DEFAULT_CONTROLLER;
+
+if (!empty($_GET['controlador'])) $controller = $_GET['controlador'];
+$action = DEFAULT_ACTION;
+if (!empty($_GET['action'])) $action = $_GET['action'];
+$controller = CONTROLLERS_FOLDER . $controller . '_controlador.php';
+
+try {
+    if (is_file($controller))
+        require_once ($controller);
+    else
+        throw new Exception('El controlador no existe - 404 not found');
+    if (is_callable($action))
+        $action();
+    else
+        throw new Exception('La accion no existe - 404 not found');
+} catch (Exception $e) {
+    console_log($e->getMessage());
+}
+?>
