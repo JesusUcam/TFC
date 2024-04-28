e87da632fd38116cf9a9cb52d148d1f87047b857
= <=>
2024-04-28 13:52:34 +0200

    Cambios en el footer

--- footer.php
+++ footer.php
@@ -4,20 +4,34 @@
     
 <div class="container">
     <div class="column">
-        <h2>Columna 1</h2>
+        <h2>Contacto</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel eros nec sapien finibus suscipit.</p>
     </div>
     <div class="column">
-        <h2>Columna 2</h2>
+        <h2>Site map</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel eros nec sapien finibus suscipit.</p>
     </div>
     <div class="column">
-        <h2>Columna 3</h2>
+        <h2>Legal</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel eros nec sapien finibus suscipit.</p>
+    </div>
+
+    <div class="column">
+        <h2>Siguenos</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel eros nec sapien finibus suscipit.</p>
     </div>
 </div>
 
+<hr size="1px" color="white" />
+<br>
+<br>
+<br>
+<p>Creado, maquetado y programado por Jesus y Alejandro</p>
 
+<br>
+<br>
+<br>
+<br>
 
 
 </footer>
